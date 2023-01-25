0786fdca13fbd85fa64dc8d32753a28e2d33b7aa
mukeshsin <110765718+mukeshsin@users.noreply.github.com>
2023-01-25 17:43:19 +0530

    completed assignment-3

---
+++ src/components/Go.vue
@@ -0,0 +1,45 @@
+<template>
+<button @click.prevent.capture="$emit('close')" class="start" v-show="!isPlaying"> Go </button>
+<button @click.prevent.capture="$emit('open')" class="stop" v-show="isPlaying"> Stop </button>
+</template>
+
+<script>
+export default {
+    name: "startMode",
+    data() {
+        return {
+
+        };
+    },
+    emit: ["close", "open"],
+    props: ["delay", "isPlaying", "gameStart", "gameStop"],
+};
+</script>
+
+<style>
+.start {
+    width: 500px;
+    height: 150px;
+    background-color: #68ae64;
+    border: none;
+    border-radius: 20px;
+    align-items: center;
+    font-size: 35px;
+    color: white;
+    margin-top: 60px;
+
+}
+
+.stop {
+    width: 500px;
+    height: 150px;
+    background-color: #c86a67;
+    border: none;
+    border-radius: 20px;
+    align-items: center;
+    font-size: 35px;
+    color: white;
+    margin-top: 60px;
+
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="result">
+    <template v-if="!isPlaying">
+        <div class="score" v-if="earlyTime">
+            <h2 v-if="score > 0">Your score is {{ score }}</h2>
+        </div>
+        <h3 class="score" v-else>click Go to test your reaction time!</h3>
+    </template>
+    <template v-else-if="isPlaying">
+        <h2 class="score">Pay attention. Click stop when the color changes.</h2>
+    </template>
+
+    <h3 class="highscoretitle">Your High Score</h3>
+    <h4 class="highscore">{{ highScore }} sec</h4>
+</div>
+</template>
+
+<script>
+export default {
+    name: "resultView",
+
+    data() {
+        return {};
+    },
+
+    props: ["score", "highScore", "isPlaying", "earlyTime"],
+};
+</script>
+
+<style>
+.result {
+    width: 500px;
+    height: 150px;
+    border-radius: 20px;
+    border: none;
+    background-color: lightskyblue;
+}
+
+.score {
+    font-size: 18px;
+    margin-left: 30px;
+}
+
+.highscoretitle {
+    font-size: 20px;
+    margin-left: 15px;
+    margin-left: 30px;
+}
+
+.highscore {
+    font-size: 20px;
+    margin-left: 15px;
+    margin-left: 30px;
+}
+</style>
